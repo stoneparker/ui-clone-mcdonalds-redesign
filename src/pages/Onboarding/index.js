93069426312cf59eace816591ccfcf640b8800e0
Vitoria Lopes <heyvitoria.lopes@gmail.com>
2020-09-28 09:51:23 -0300

    first commit

---
+++ src/pages/Onboarding/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const Onboarding = () => {
+  return <View />;
+}
+
+export default Onboarding;
